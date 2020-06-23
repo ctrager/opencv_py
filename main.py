d033f9020cf6d3361aefb039ae8a81cee4847032
corey <ctrager@yahoo.com>
2020-06-22 22:48:35 -0500

    removed background subtractor

--- main.py
+++ main.py
@@ -5,23 +5,11 @@ import cv2
 class Config:
     g1 = 21
     g2 = 21
-    t1 = 127
-    t2 = 255
+    t1 = 20
     scale_factor = .5
-    background_history = 200
-    background_thresh = 16
-    background_detect_shadows = True
+    new_size = [0,0]
+    pixel_count = 0
 
-back_sub = None
-
-def create_back_sub():
-    bs = cv2.createBackgroundSubtractorMOG2(
-        Config.background_history, Config.background_thresh, Config.background_detect_shadows)
-    return bs
-
-
-def myfunc(arg):
-    print(arg)
 
 def handle_gauss1(arg1):
     Config.g1 = arg1
@@ -36,64 +24,40 @@ def handle_gauss2(arg1):
 def handle_thresh1(arg1):
     Config.t1 = arg1
 
-def handle_thresh2(arg1):
-    Config.t2 = arg1
-
-def handle_back1(arg1):
-    global back_sub
-    Config.background_history = arg1
-    back_sub = create_back_sub()
-
-def handle_back2(arg1):
-    global back_sub
-    Config.background_thresh = arg1
-    back_sub = create_back_sub()
-
-#myfunc("corey")
-
-
-video_capture = cv2.VideoCapture(0)
-
+# build windows
 cv2.namedWindow("window1", cv2.WINDOW_AUTOSIZE)
 
 cv2.createTrackbar("gauss1", "window1", 0, 255, handle_gauss1)
 cv2.createTrackbar("gauss2", "window1", 0, 255, handle_gauss2)
-
 cv2.createTrackbar('thresh1', 'window1', 0, 255, handle_thresh1)
-cv2.createTrackbar('thresh2', 'window1', 0, 255, handle_thresh2)
-
-cv2.createTrackbar('back1', 'window1', 0, 200, handle_back1)
-cv2.createTrackbar('back2', 'window1', 0, 255, handle_back2)
-
 
 cv2.setTrackbarPos("gauss1", "window1", Config.g1)
 cv2.setTrackbarPos("gauss2", "window1", Config.g2)
-
 cv2.setTrackbarPos("thresh1", "window1", Config.t1)
-cv2.setTrackbarPos("thresh2", "window1", Config.t2)
 
-cv2.setTrackbarPos("back1", "window1", Config.background_history)
-cv2.setTrackbarPos("back2", "window1", Config.background_thresh)
+prev_frame = []
 
+video_capture = cv2.VideoCapture(0)
+
+# get first frame
 ret, frame = video_capture.read()
-
-back_sub = create_back_sub()
-
-mask = back_sub.apply(frame)
 
 # for scaling
 width = len(frame[0])
 height = len(frame)
 print (width, height)
 
-new_size = (int(width * Config.scale_factor), int(height * Config.scale_factor))
+Config.new_size = (int(width * Config.scale_factor), int(height * Config.scale_factor))
+Config.pixel_count = Config.new_size[0] * Config.new_size[1]
 
-prev_frame = []
+nth = 0
 
 while(True):
-     
+
+    # nth += 1
+
     # resize
-    frame = cv2.resize(frame, new_size)
+    frame = cv2.resize(frame, Config.new_size)
 
     # gray scale
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -108,19 +72,19 @@ while(True):
         images = np.hstack([frame, bgr_gray_blurred_frame])
     else:
         diff = cv2.absdiff(blurred_frame, prev_frame)
-        thresh = cv2.threshold(diff, Config.t1, Config.t2, cv2.THRESH_BINARY)[1]
+        thresh = cv2.threshold(diff, Config.t1, 255, cv2.THRESH_BINARY)[1]
+
+        pixels_with_motion_count = cv2.countNonZero(thresh)
+        print(pixels_with_motion_count, Config.pixel_count, 
+            round(pixels_with_motion_count/Config.pixel_count * 100))
+
         row1 = np.hstack([frame, bgr_gray_blurred_frame])
         diff = cv2.cvtColor(diff, cv2.COLOR_GRAY2BGR)
         thresh = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+
         row2 = np.hstack([diff, thresh])
         images = np.vstack([row1, row2])
-
-        # new way
-        mask = back_sub.apply(frame)
-        #images = np.hstack([frame, cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)])
-
-    # arrange arrays side by side
- 
+   
     # display it
     cv2.imshow("window1",images)
  

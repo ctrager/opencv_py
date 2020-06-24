b16266d7cac93a0ce11c7a6d104193c856dac3b1
corey <ctrager@yahoo.com>
2020-06-24 08:38:50 -0500

    state changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 print("hello")
 import numpy as np
 import cv2
+import time
 
 class Config:
     gauss_blur = 25
@@ -12,10 +13,18 @@ class Config:
     saturation = 100
     yellow_green = 30
     blue_green = 100
+    interval_in_milliseconds = 400
+    motion_threshold_percent = 10
+    recording_length_in_seconds = 3
+    cooldown_in_seconds = 10
 
 BLUE = 0
 GREEN = 1
 RED = 2
+ORIGINAL_FRAME = 0
+DIFF_FRAME = 1
+
+state = "none"
 
 def handle_gauss_blur(arg1):
     Config.gauss_blur = arg1
@@ -34,6 +43,49 @@ def handle_yellow_green(arg1):
 def handle_blue_green(arg1):
     Config.blue_green = arg1
 
+def current_milliseconds():
+    return round(time.time() * 1000)
+
+def change_state(new_state):
+    global state
+    print(current_milliseconds(), "state", state, new_state)
+    state = new_state
+
+def process_frame(frame):
+    
+    # resize
+    small_frame = cv2.resize(frame, Config.new_size)
+    
+    # blur
+    blurred_frame = cv2.GaussianBlur(small_frame,
+        (Config.gauss_blur, Config.gauss_blur), cv2.BORDER_CONSTANT)
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+
+    # red to yellow, but not into green
+    in_range1 = cv2.inRange(hsv, 
+        (0, Config.saturation, Config.lightness), 
+        (Config.yellow_green, 255, 255))
+    
+    # red to maybe turqoise, but not into green
+    in_range2 = cv2.inRange(hsv, 
+        (Config.blue_green, Config.saturation, Config.lightness), 
+        (180, 255, 255))
+
+    in_range_all_gray = cv2.bitwise_or(in_range1, in_range2)
+
+    #in_range1 = cv2.cvtColor(in_range1, cv2.COLOR_GRAY2BGR)
+    #in_range2 = cv2.cvtColor(in_range2, cv2.COLOR_GRAY2BGR)
+    #in_range_all_bgr = cv2.cvtColor(in_range_all_gray, cv2.COLOR_GRAY2BGR)
+
+    # return an array of frames
+    # "original" is index 0
+    # the one for diffing is index 1
+    # others appended
+    return [small_frame, in_range_all_gray]
+
+
 prev_frame = []
 
 # build windows
@@ -51,7 +103,6 @@ cv2.setTrackbarPos("saturation", "window1", Config.saturation)
 cv2.setTrackbarPos("yellow_green", "window1", Config.yellow_green)
 cv2.setTrackbarPos("blue_green", "window1", Config.blue_green)
 
-
 video_capture = cv2.VideoCapture(0)
 
 # get first frame
@@ -65,39 +116,51 @@ print (width, height)
 Config.new_size = (int(width * Config.scale_factor), int(height * Config.scale_factor))
 Config.pixel_count = Config.new_size[0] * Config.new_size[1]
 
-nth = 0
+prev_frame = process_frame(frame)[DIFF_FRAME]
+delta_frame_gray = prev_frame
+
+prev_time = current_milliseconds()
+
+recording_start_time = 0
 
 while(True):
 
-    # nth += 1
+    now = current_milliseconds()
 
-    # resize
-    frame = cv2.resize(frame, Config.new_size)
-    blurred_frame = cv2.GaussianBlur(frame, (Config.gauss_blur, Config.gauss_blur), cv2.BORDER_CONSTANT)
+    altered_frames = process_frame(frame)
 
 
-     # Convert BGR to HSV
-    hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+    if now - prev_time > Config.interval_in_milliseconds:
+        # how much has changed
+        delta_frame_gray = cv2.absdiff(altered_frames[DIFF_FRAME], prev_frame)
+        pixels_with_motion_count = cv2.countNonZero(delta_frame_gray)
 
-    # hue (0 to 180), saturation, brightness
-    # red to yellow, but not into gree
-    in_range1 = cv2.inRange(hsv, 
-        (0, Config.saturation, Config.lightness), 
-        (Config.yellow_green, 255, 255))
-    in_range2 = cv2.inRange(hsv, 
-        (Config.blue_green, Config.saturation, Config.lightness), 
-        (180, 255, 255))
-    in_range_all = cv2.bitwise_or(in_range1, in_range2)
+        motion_percent = round(pixels_with_motion_count/Config.pixel_count * 100)
+        #print(pixels_with_motion_count, Config.pixel_count, motion_percent)
+  
+        if motion_percent > Config.motion_threshold_percent:
+            change_state("recording")
+            print(state)
+            recording_start_time = now
 
-    in_range1 = cv2.cvtColor(in_range1, cv2.COLOR_GRAY2BGR)
-    in_range2 = cv2.cvtColor(in_range2, cv2.COLOR_GRAY2BGR)
-    in_range_all = cv2.cvtColor(in_range_all, cv2.COLOR_GRAY2BGR)
-        
-    row1 = np.hstack([in_range1, in_range2])
-    row2 = np.hstack([frame, in_range_all])
-    images = np.vstack([row1, row2])
+        prev_frame = altered_frames[DIFF_FRAME]
+        prev_time = now
 
-    cv2.imshow('window1',images)
+    display_image = np.hstack([
+        altered_frames[ORIGINAL_FRAME], 
+        cv2.cvtColor(altered_frames[DIFF_FRAME], cv2.COLOR_GRAY2BGR),
+        cv2.cvtColor(delta_frame_gray, cv2.COLOR_GRAY2BGR)])
+    
+    cv2.imshow('window1',display_image)
+
+    if state == "recording":
+        if now - recording_start_time > (Config.recording_length_in_seconds * 1000):
+            # finish recording
+            change_state("none")
+            print(state)
+        else:
+            # continue recording
+            pass
 
     # detect window closing
     key = cv2.waitKey(1) 
@@ -109,7 +172,6 @@ while(True):
         break   
 
     # get next frame
-    prev_frame = frame
     ret, frame = video_capture.read()
 
 # clean up

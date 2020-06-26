525efc404d27d60142e8391551515e298870388f
corey <ctrager@yahoo.com>
2020-06-26 15:48:07 -0500

    detection areas

--- pretty_birds.py
+++ pretty_birds.py
@@ -37,6 +37,7 @@ class Config:
     new_size_for_analysis = (310,300) # 1/3 size
     new_size_for_video = (620,600) # 2/2 size
     pixel_count = 310 * 300
+    rect_points = ((60,100),(134,280),(186,100),(260,280))
 
 BLUE = 0
 GREEN = 1
@@ -133,7 +134,16 @@ def process_frame(frame):
     
     both = cv2.bitwise_or(gray_without_red_bgr, just_red)
 
+    #cv2.rectangle(small_frame, (60,100), (134, 280), (255,255,0), 1)
+    #cv2.rectangle(small_frame, (186,100), (260, 280), (255,255,0), 1)
 
+    upper_left = Config.rect_points[0]
+    bottom_right = Config.rect_points[1]
+    cv2.rectangle(small_frame, upper_left, bottom_right, (255,255,0), 1)
+    upper_left = Config.rect_points[2]
+    bottom_right = Config.rect_points[3]
+    cv2.rectangle(small_frame, upper_left, bottom_right, (255,255,0), 1)
+ 
     # return an array of frames
     # "original" is index 0
     # the one for diffing is index 1
@@ -176,6 +186,15 @@ delta_bgr = prev_frame
 
 prev_time = current_milliseconds()
 
+def measure_motion(upper_left, lower_right, curr, prev):
+    delta = cv2.absdiff(
+        altered_frames[DIFF_FRAME][upper_left[1]:lower_right[1], upper_left[0]:lower_right[0]], 
+        prev_frame[upper_left[1]:lower_right[1], upper_left[0]:lower_right[0]])
+    pixels_with_motion_count = cv2.countNonZero(cv2.cvtColor(delta, cv2.COLOR_BGR2GRAY))
+    pixel_count = (upper_left[0]-lower_right[0]) * (upper_left[1]-lower_right[1])
+    #print(pixels_with_motion_count, pixel_count)
+    return pixels_with_motion_count, pixel_count
+
 while(True):
 
     now = current_milliseconds()
@@ -187,10 +206,17 @@ while(True):
   
         delta_bgr = cv2.absdiff(altered_frames[DIFF_FRAME], prev_frame)
         delta_gray = cv2.cvtColor(delta_bgr, cv2.COLOR_BGR2GRAY)
-        pixels_with_motion_count = cv2.countNonZero(delta_gray)
 
-        motion_percent = round(pixels_with_motion_count/Config.pixel_count * 100)
-  
+        pixels_with_motion_count_1, pixel_count_1 = measure_motion(
+            Config.rect_points[0], Config.rect_points[1], altered_frames[DIFF_FRAME], prev_frame)
+        pixels_with_motion_count_2, pixel_count_2 = measure_motion(
+            Config.rect_points[0], Config.rect_points[1], altered_frames[DIFF_FRAME], prev_frame)
+       
+        pixels_with_motion_count = pixels_with_motion_count_1 + pixels_with_motion_count_2
+        pixel_count = pixel_count_1 + pixel_count_2
+        motion_percent = round(pixels_with_motion_count/pixel_count * 100)
+        print(pixels_with_motion_count, motion_percent, pixel_count)
+    
         if state == STATE_NONE:
             if Config.create_video == 1 and motion_percent > Config.motion_threshold_percent:
                 change_state(STATE_RECORDING)

2e0b8822d47b324e3a3868273c696d19f345995c
corey <ctrager@yahoo.com>
2020-07-16 16:02:44 -0500

    narrower blue

--- red_yellow_blue.py
+++ red_yellow_blue.py
@@ -15,7 +15,7 @@ class Config:
     red1 = (166,180)
     red2 = (0, 14)
     yellow = (25,32)
-    blue = (105, 135)
+    blue = (110, 125)
     interval_in_milliseconds = 400
     red_threshold_percent = 20
     yellow_threshold_percent = 20

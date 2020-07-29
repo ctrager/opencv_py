24654467cf613ca8183ff3f301b74637abc25667
corey <ctrager@yahoo.com>
2020-07-29 15:42:53 -0500

    thread

--- neolink_threaded.py
+++ neolink_threaded.py
@@ -7,7 +7,8 @@ class Config:
     url = "rtsp://10.0.0.8:8554/one"
 #    url = "rtsp://127.0.0.1:8554/one"
     framerate = 12
-    queue_size = 36
+    queue_size = framerate * 50
+    gauss_blur = 5
     interval_in_milliseconds = 0
     motion_threshold_percent = 30
     recording_length_in_seconds = 15
@@ -28,10 +29,10 @@ class Config:
     rect_bottom = 432
 
 queue = []
-
-lock =threading.Lock()
-
-video_capture = cv2.VideoCapture(Config.url)
+video_capture = None
+lock = threading.Lock()
+stopping = False
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 def append_to_queue(queue, item):
     lock.acquire()
@@ -50,37 +51,92 @@ def queue_len(queue):
     lock.release()
     return size
 
-
-def thread_func():    
+def read_feed():    
     producer_cnt = 0
     frame = video_capture.grab()
     while (True):
+        if stopping == True:
+            break
         frame = video_capture.grab()
         if frame != None:
-            producer_cnt += 1
-            if queue_len(queue) > Config.queue_size:
-                pop_from_queue(queue)
-            
             append_to_queue(queue, frame)
-            print("producer", producer_cnt)
+    print("exiting read_feed thread")
  
-
-mythread = threading.Thread(target=thread_func, args=())
-mythread.start()
+#start
+video_capture = cv2.VideoCapture(Config.url)
+read_feed_thread = threading.Thread(target=read_feed, args=())
+read_feed_thread.start()
+prev_frame = []
+diff_frame = []
+motion_percent = 0
+state = ""
 
 consumer_cnt = 0
+
+
 while(True):
     while (True):
         if queue_len(queue) > 0:
             frame = pop_from_queue(queue)
+            ret,decoded = video_capture.retrieve(frame)
+            if ret == False:
+                print("skipping frame")
+                continue
             break
         else:
-            time.sleep(.1)
+            time.sleep(.04)
     consumer_cnt += 1
-    ret,decoded = video_capture.retrieve(frame)
-    resized = cv2.resize(decoded, Config.new_size_for_display)
-    cv2.imshow("w",resized)
-    cv2.waitKey(1)
-    print("consumer", consumer_cnt, np.sum(decoded, dtype=np.int64))
-    #time.sleep(.2)
+    
+    resized_for_display = cv2.resize(decoded, Config.new_size_for_display)
+
+    if consumer_cnt % 5 == 0:
+        cropped = resized_for_display[Config.rect_top:Config.rect_bottom, Config.rect_left:Config.rect_right]
+        blur_frame  = cv2.GaussianBlur(cropped, (Config.gauss_blur, Config.gauss_blur), cv2.BORDER_CONSTANT)
+        eroded = cv2.erode(blur_frame, Config.kernel)
+        dilated = cv2.dilate(eroded, Config.kernel)
+        gray = cv2.cvtColor(dilated, cv2.COLOR_BGR2GRAY)
+        diff_frame = gray
+        if len(prev_frame) == 0:
+            prev_frame = diff_frame
+
+        # # how much has changed
+        try:
+            diff = cv2.absdiff(diff_frame, prev_frame)
+            sum_diff = np.sum(diff, dtype=np.int64)
+            sum_prev_frame = np.sum(prev_frame, dtype=np.int64)
+            pct = sum_diff/sum_prev_frame * 5
+            motion_percent = int(round(pct * 100))
+        except:
+            motion_percent = 0            
+
+        # calc score here
+
+
+        #cv2.imshow("d", gray)
+    
+    # text on image
+    text_on_image = str(motion_percent) # + "," + state + "," + str(fps)
+    cv2.putText(resized_for_display, text_on_image,
+         (10, len(resized_for_display) - 20  ), font, 
+         .8, (255,255,0), 2, cv2.LINE_AA)
+
+    cv2.imshow("window1", resized_for_display)
+
+    # detect window closing
+    key = cv2.waitKey(1) 
+
+    if key == 27: # escape key
+        break
+
+    if cv2.getWindowProperty("window1",cv2.WND_PROP_AUTOSIZE)  < 1: # closed with X       
+        break   
+
+    prev_frame = diff_frame
+
+# clean up
+print("clean up")
+stopping = True
+read_feed_thread.join()
+video_capture.release()
+cv2.destroyAllWindows()
 print("goodbye")
